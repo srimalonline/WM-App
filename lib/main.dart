650acaff1e1ba0fb60b0683bcd133013cd8fb49d
Srimal Fernando <67434227+srimalonline@users.noreply.github.com>
2023-03-24 23:55:45 +0530

    theme adding

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,12 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Text(pair.asLowerCase),
